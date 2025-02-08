98e1da198fc300f1fe3e24cd4abcf668388ab549
RevathiDampuri <ndampuri@pnw.edu>
2025-02-08 16:38:29 -0600

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,8 @@
-﻿double result = Calculator.Core.Calculator.Divide(2, 1);
-Console.WriteLine($"Result is {result}");
+﻿double r1 = Calculator.Core.Calculator.Divide(2, 1);
+Console.WriteLine($"Result is {r1}");
+﻿double r2 = Calculator.Core.Calculator.Add(3,6);
+Console.WriteLine($"Result is {r2}");
+﻿double r3 = Calculator.Core.Calculator.Multiply(4,6);
+Console.WriteLine($"Result is {r3}");
+﻿double r4 = Calculator.Core.Calculator.Subtract(3,2);
+Console.WriteLine($"Result is {r4}");
